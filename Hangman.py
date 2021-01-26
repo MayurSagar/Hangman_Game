311fbfae01eb0449a2e25faabdd1560bff0761a1
mayur sagar <mayursagar1105@gmail.com>
2021-01-26 13:58:19 +0530

    Checking mouse position and loading images

--- Hangman.py
+++ Hangman.py
@@ -1,14 +1,25 @@
 import pygame
 
+# ***** Setup Display ****
 pygame.init()
 
 # Defining Width and Height of our Game Window
 WIDTH, HEIGHT = 800, 500
+
 # Display width and height in Tuple.Because pygame Display width and height as Tuple.
-pygame.display.set_mode((WIDTH, HEIGHT))
+# Storing the display
+win =pygame.display.set_mode((WIDTH, HEIGHT))
 
 # Define and Display Game name
 pygame.display.set_caption("Hangman Game!")
+
+# ***** Load Images *****
+images = []
+for i in range(7):
+    image = pygame.image.load("hangman"+ str(i) + ".png")
+    images.append(image)
+
+# ***** Game Loop *****
 
 # Frame Per Second
 FPS = 60
@@ -26,6 +37,9 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            pos = pygame.mouse.get_pos()
+            print(pos)
 
 
 pygame.quit()
