faf5ad0ee07bd0093765607ea31dbcae50abccbd
mayur sagar <mayursagar1105@gmail.com>
2021-01-28 14:22:16 +0530

    Button Collision

--- Hangman.py
+++ Hangman.py
@@ -1,5 +1,5 @@
 import pygame
-
+import math
 # ***** Setup Display ****
 pygame.init()
 
@@ -34,7 +34,7 @@ for i in range(26):
     # x and y position of buttons
     x = startx + GAP * 2 + ((RADIUS * 2 + GAP) * (i % 13))
     y = starty + (i // 13) * (GAP + RADIUS * 2)
-    letters.append([x, y, chr(A + i)])
+    letters.append([x, y, chr(A + i), True])
 
 # ***** Game Variable *****
 # for knowing the status of hangman images which we are using.
@@ -53,12 +53,13 @@ def draw():
     win.fill(WHITE)
     # draw buttons on x, y position and letter which we want to draw
     for letter in letters:
-        x, y, ltr = letter
-        pygame.draw.circle(win, BLACK, (x, y), RADIUS, 3)
-        # render text means what text you want to get displayed
-        text = LETTER_FONT.render(ltr, 1, BLACK)
-        # draw text on x, y position
-        win.blit(text, (x -text.get_width() / 2 , y - text.get_height() / 2 ))
+        x, y, ltr, visible = letter
+        if visible:
+            pygame.draw.circle(win, BLACK, (x, y), RADIUS, 3)
+            # render text means what text you want to get displayed
+            text = LETTER_FONT.render(ltr, 1, BLACK)
+            # draw text on x, y position
+            win.blit(text, (x -text.get_width() / 2 , y - text.get_height() / 2 ))
 
     win.blit(images[hangman_status], (150, 100))
     pygame.display.update()
@@ -76,7 +77,14 @@ while run:
         if event.type == pygame.QUIT:
             run = False
         if event.type == pygame.MOUSEBUTTONDOWN:
-            pos = pygame.mouse.get_pos()
-            print(pos)
+            m_x , m_y = pygame.mouse.get_pos()
+            for letter in letters:
+                x, y, ltr , visible = letter
+                if visible:
+                    # distance between x, y cordinate and mourse x, y coordinate m_x, m_y)
+                    distance = math.sqrt((x - m_x)** 2 + ( y - m_y) ** 2)
+                    if distance < RADIUS:
+                        letter[3] = False
+
 
 pygame.quit()
